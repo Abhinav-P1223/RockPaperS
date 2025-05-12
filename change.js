7107df6bfc7ec6ccb3b6bc4179ca6de94c994530
Abhinava Satya Sai <rlintern36@rlpl.com>
2025-05-12 12:29:24 +0530

    Version1 restored

--- change.js
+++ change.js
@@ -1,3 +1,1 @@
-console.log ("This is for reference of it.");
-console.log ("This is for Visualization purpose to track the diff versions");
-console.log("Version 3");
+console.log ("This is for reference of git.");
